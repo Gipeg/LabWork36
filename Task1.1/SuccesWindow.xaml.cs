292a0e585190de759f3594c6783bc4d9b28759b4
fisp <arenatkanovna@gmail.com>
2024-06-23 21:03:27 +0300

    Добавьте файлы проекта.

---
+++ Task1.1/SuccesWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для SuccesWindow.xaml
+    /// </summary>
+    public partial class SuccesWindow : Window
+    {
+        public SuccesWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void yesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void noButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult= false;
+        }
+    }
+}
